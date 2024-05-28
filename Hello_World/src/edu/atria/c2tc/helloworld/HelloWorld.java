fcaf2ff04a7995e04ddff54922426aff9ee3b85d
Spoorthi S <sspoorthi328@gmial.com>
2024-05-28 09:25:42 +0530

    first commit

---
+++ Hello_World/src/edu/atria/c2tc/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package edu.atria.c2tc.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
